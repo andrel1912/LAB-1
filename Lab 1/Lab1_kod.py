1e0f932ab02daccac6b884454737f5775e7ae083
andrel1912 <andreas.holmberg@stud.sti.se>
2025-10-16 20:52:35 +0200

    Sekvenser

--- Lab 1/Lab1_kod.py
+++ Lab 1/Lab1_kod.py
@@ -7,4 +7,11 @@ filnamn = input("Ange filnamn:")
 # Läser in dna-filen 
 with open(filnamn, encoding="utf-8") as f:
     rader = [rad.strip() for rad in f if rad.strip()] # Den här delen är till för att ta bort tomma rader
-    sekvenser = 
+    sekvenser = [(rader[i][1:], rader[i+1].upper()) for i in range(0, len(rader), 2)]
+
+    print ("/nResultat av DNA-analys/n")
+
+    # Analyserar sekvenserna
+    for namn, sekvens in sekvenser:
+        # Räknar baser
+        baser = 

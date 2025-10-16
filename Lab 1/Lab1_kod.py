a4a34dd743350f8adf5e20575c7dbab4d1d54c35
andrel1912 <andreas.holmberg@stud.sti.se>
2025-10-16 13:39:59 +0200

    början av min kod huvudprogrammet m.m.

--- Lab 1/Lab1_kod.py
+++ Lab 1/Lab1_kod.py
@@ -1,2 +1,10 @@
 import matplotlib.pyplot as plt
-print("Hello, world!")
+
+# Huvudprogrammet (Man skriver in filen du vill ska läsas)
+
+filnamn = input("Ange filnamn:")
+
+# Läser in dna-filen 
+with open(filnamn, encoding="utf-8") as f:
+    rader = [rad.strip() for rad in f if rad.strip()] # Den här delen är till för att ta bort tomma rader
+    sekvenser = 
